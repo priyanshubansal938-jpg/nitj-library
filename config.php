68ecc8d4f9359183de2edaba7d3afe1a3189181b
Priyanshu Bansal <priyanshubansal641@gmail.com>
2026-04-24 23:04:48 +0530

    add config with env variables

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'library_db');
+define('DB_PORT', getenv('DB_PORT') ?: 3306);
